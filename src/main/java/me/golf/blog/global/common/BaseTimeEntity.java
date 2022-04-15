a4e961c71337400b65d3018862c4b8d1f84daf7a
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-04-16 01:15:23 +0900

    "생성시간과 수정시간은 Entity간 중복되기 때문에 상속으로 중복 코드를 제거한다."

---
+++ src/main/java/me/golf/blog/global/common/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package me.golf.blog.global.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "create_time", updatable = false)
+    private LocalDateTime createTime;
+
+    @LastModifiedBy
+    @Column(name = "deleted_time")
+    private LocalDateTime deletedTime;
+}
