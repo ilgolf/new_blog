c2e73ead4b590b2b05a1a6dd391043e8706b2f0c
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-04-27 21:44:30 +0900

    "오타 수정"

--- src/main/java/me/golf/blog/domain/member/domain/vo/Email.java
+++ src/main/java/me/golf/blog/domain/member/domain/vo/Email.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.validation.constraints.NotBlank;
 import java.util.Objects;
 
 @Embeddable
