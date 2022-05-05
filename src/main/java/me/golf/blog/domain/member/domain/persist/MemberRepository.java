0be2fe762252a3779777674744fccb677e400f5e
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-05-05 15:57:19 +0900

    "회원 인증관리 서비스 구현 완료 : token 인증 방식"

--- src/main/java/me/golf/blog/domain/member/domain/persist/MemberRepository.java
+++ src/main/java/me/golf/blog/domain/member/domain/persist/MemberRepository.java
@@ -1,6 +1,10 @@
 package me.golf.blog.domain.member.domain.persist;
 
+import me.golf.blog.domain.member.domain.vo.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(final Email email);
 }
