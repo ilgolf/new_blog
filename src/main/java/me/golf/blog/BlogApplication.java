c27ceeadbff2ca03aaa3ab136428a357b8eb8e01
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-05-16 23:11:43 +0900

    "캐싱 전략을 사용하기 위한 Cache 설정"

--- src/main/java/me/golf/blog/BlogApplication.java
+++ src/main/java/me/golf/blog/BlogApplication.java
@@ -2,14 +2,15 @@ package me.golf.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
-
 }
