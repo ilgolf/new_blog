6583d1fe688c4c0817af53872150b30cf0e1edbb
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-05-16 22:38:23 +0900

    "게시판 생성 및 업데이트 시 히스토리관리를 위한 BaseEntity 추가"

--- src/main/java/me/golf/blog/domain/board/domain/persist/Board.java
+++ src/main/java/me/golf/blog/domain/board/domain/persist/Board.java
@@ -1,20 +1,26 @@
 package me.golf.blog.domain.board.domain.persist;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+import me.golf.blog.domain.board.domain.vo.BoardImage;
 import me.golf.blog.domain.board.domain.vo.Content;
 import me.golf.blog.domain.board.domain.vo.Title;
+import me.golf.blog.domain.boardCount.domain.persist.BoardCount;
 import me.golf.blog.domain.member.domain.persist.Member;
-import me.golf.blog.global.common.BaseTimeEntity;
+import me.golf.blog.global.common.BaseEntity;
 
 import javax.persistence.*;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "Board", indexes = {
-        @Index(name = "index_title", columnList = "title", unique = true)
+        @Index(name = "index_title", columnList = "title")
 })
-public class Board extends BaseTimeEntity {
+public class Board extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id", nullable = false)
@@ -26,28 +32,27 @@ public class Board extends BaseTimeEntity {
     @Embedded
     private Content content;
 
-    private int view;
+    @OneToOne(mappedBy = "board", fetch = FetchType.EAGER)
+    private BoardCount boardCount;
+
+    @Embedded
+    private BoardImage boardImage;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    private Board(Title title, Content content, int view) {
-        this.title = title;
-        this.content = content;
-        this.view = view;
-    }
-
     public void addMember(final Member member) {
         this.member = member;
-    }
-
-    public static Board of(final Title title, final Content content, final int view) {
-        return new Board(title, content, view);
+        member.addBoard(this);
     }
 
     public void updateBoard(final Board board) {
         this.title = board.getTitle();
         this.content = board.getContent();
+    }
+
+    public void addBoardCount(final BoardCount boardCount) {
+        this.boardCount = boardCount;
     }
 }
