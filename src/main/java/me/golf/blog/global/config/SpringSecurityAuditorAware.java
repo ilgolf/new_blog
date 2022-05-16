c210c80857c47cd7eb5b518df67ca2d733852dc0
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-05-17 08:32:02 +0900

    "Audit 커스텀하여 Security 정보에서 회원의 PK를 저장한다."

---
+++ src/main/java/me/golf/blog/global/config/SpringSecurityAuditorAware.java
@@ -0,0 +1,28 @@
+package me.golf.blog.global.config;
+
+import me.golf.blog.global.security.principal.CustomUserDetails;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Configuration
+public class SpringSecurityAuditorAware implements AuditorAware<Long> {
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .map(authentication -> {
+                    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+                    boolean isUser = authorities.contains(new SimpleGrantedAuthority("USER"));
+                    CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
+
+                    return isUser ? principal.getId() : null;
+                });
+    }
+}
