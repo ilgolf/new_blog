9f4a9cf9012dbbc9079548f91a0c3d7343a34003
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-05-22 18:07:39 +0900

    "Email을 ~by에 저장"

--- src/main/java/me/golf/blog/global/config/SpringSecurityAuditorAware.java
+++ src/main/java/me/golf/blog/global/config/SpringSecurityAuditorAware.java
@@ -1,5 +1,6 @@
 package me.golf.blog.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import me.golf.blog.global.security.principal.CustomUserDetails;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -11,18 +12,21 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import java.util.Collection;
 import java.util.Optional;
 
+@Slf4j
 @Configuration
-public class SpringSecurityAuditorAware implements AuditorAware<Long> {
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
     @Override
-    public Optional<Long> getCurrentAuditor() {
+    public Optional<String> getCurrentAuditor() {
         return Optional.ofNullable(SecurityContextHolder.getContext())
                 .map(SecurityContext::getAuthentication)
                 .map(authentication -> {
                     Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
                     boolean isUser = authorities.contains(new SimpleGrantedAuthority("USER"));
-                    CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
-
-                    return isUser ? principal.getId() : null;
+                    if (isUser) {
+                        CustomUserDetails principal = (CustomUserDetails) authentication.getPrincipal();
+                        return principal.getUsername();
+                    }
+                    return null;
                 });
     }
 }
