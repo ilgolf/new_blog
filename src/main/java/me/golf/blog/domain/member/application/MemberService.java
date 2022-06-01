5d43b4ff8e68fd221000154cd4d3460d6d5be657
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-06-01 21:58:56 +0900

    "회원 delete 시 논리적으로만 삭제한다."

--- src/main/java/me/golf/blog/domain/member/application/MemberService.java
+++ src/main/java/me/golf/blog/domain/member/application/MemberService.java
@@ -1,25 +1,30 @@
 package me.golf.blog.domain.member.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.golf.blog.domain.member.domain.persist.Member;
+import me.golf.blog.domain.member.domain.persist.MemberQueryRepository;
 import me.golf.blog.domain.member.domain.persist.MemberRepository;
 import me.golf.blog.domain.member.dto.JoinResponse;
 import me.golf.blog.domain.member.dto.MemberDTO;
-import me.golf.blog.domain.member.dto.MemberResponse;
 import me.golf.blog.domain.member.error.MemberNotFoundException;
 import me.golf.blog.domain.memberCount.application.MemberCountService;
-import me.golf.blog.domain.memberCount.domain.persist.MemberCountRepository;
 import me.golf.blog.global.error.exception.ErrorCode;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder encoder;
     private final MemberCountService memberCountService;
+    private final MemberQueryRepository memberQueryRepository;
 
     // create
     public JoinResponse create(final Member member) {
@@ -29,20 +34,27 @@ public class MemberService {
 
     // find
     @Cacheable(key = "#memberId", value = "getMember")
+    @Transactional(readOnly = true)
     public MemberDTO getMember(final Long memberId) {
-        return memberRepository.findById(memberId)
-                .map(MemberDTO::of)
-                .orElseThrow(() -> new MemberNotFoundException(ErrorCode.USER_NOT_FOUND));
+        log.debug("getMember");
+        return memberQueryRepository.findByIdWithQuery(memberId).orElseThrow(
+                () -> new MemberNotFoundException(ErrorCode.USER_NOT_FOUND));
     }
 
     // update
     public void update(final Member updateMember, final Long id) {
         memberRepository.findById(id).orElseThrow(
-                () -> new MemberNotFoundException(ErrorCode.USER_NOT_FOUND)).update(updateMember);
+                () -> new MemberNotFoundException(ErrorCode.USER_NOT_FOUND)).update(updateMember, encoder);
     }
 
     // delete
-    public void delete(final Long id) {
-        memberRepository.deleteById(id);
+    public void delete(final Long memberId) {
+        deleteCache(memberId);
+        memberRepository.updateActivatedById(memberId);
+    }
+
+    @CacheEvict(value = "getMember", key = "#memberId")
+    public void deleteCache(final Long memberId) {
+        log.debug("회원 캐시 삭제 : {}", memberId);
     }
 }
--- src/main/java/me/golf/blog/domain/member/domain/persist/MemberRepository.java
+++ src/main/java/me/golf/blog/domain/member/domain/persist/MemberRepository.java
@@ -1,10 +1,18 @@
 package me.golf.blog.domain.member.domain.persist;
 
-import me.golf.blog.domain.member.domain.vo.Email;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(final Email email);
+    @EntityGraph(attributePaths = "memberCount")
+    Optional<Member> findById(final Long id);
+
+    @Modifying
+    @Query("update Member m set m.activated = false where m.id = :id")
+    void updateActivatedById(@Param("id") Long id);
 }
