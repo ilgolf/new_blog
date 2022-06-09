64fb2cc362bf6bcfe32069cecff0332e20d15d98
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-06-09 20:44:48 +0900

    "댓글 Entity 연관관계 추가 및 생성메소드 추가"

---
+++ src/main/java/me/golf/blog/domain/reply/domain/persist/Reply.java
@@ -0,0 +1,52 @@
+package me.golf.blog.domain.reply.domain.persist;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.golf.blog.domain.board.domain.persist.Board;
+import me.golf.blog.domain.member.domain.persist.Member;
+import me.golf.blog.domain.reply.domain.vo.Comment;
+import me.golf.blog.global.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Reply extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_id", nullable = false)
+    private Long id;
+
+    @Embedded
+    private Comment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    public static Reply createReply(final Comment comment, final Member member, final Board board) {
+        Reply reply = new Reply();
+
+        reply.member = member;
+        reply.addBoard(board);
+        reply.comment = comment;
+
+        return reply;
+    }
+
+    public void addBoard(final Board board) {
+        board.addReply(this);
+        this.board = board;
+    }
+
+    public void updateComment(final Comment comment) {
+        this.comment = comment;
+    }
+}
