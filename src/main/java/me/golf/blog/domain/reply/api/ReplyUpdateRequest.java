0ee2733bd72ef8c3c362cffd1056f7d55a5e97b2
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-06-10 18:16:09 +0900

    "AllArgs 생성자 추가"

--- src/main/java/me/golf/blog/domain/reply/api/ReplyUpdateRequest.java
+++ src/main/java/me/golf/blog/domain/reply/api/ReplyUpdateRequest.java
@@ -1,12 +1,14 @@
 package me.golf.blog.domain.reply.api;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.golf.blog.domain.reply.domain.vo.Comment;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class ReplyUpdateRequest {
     private Comment comment;
 }
