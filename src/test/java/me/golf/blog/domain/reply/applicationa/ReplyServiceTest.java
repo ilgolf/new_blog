5b9e3ec881ee8ed3960f0f6c0e27b317596223a9
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-06-10 18:32:26 +0900

    "ReplyService에 대한 테스트 구현 및 성공"

--- src/test/java/me/golf/blog/domain/reply/applicationa/ReplyServiceTest.java
+++ src/test/java/me/golf/blog/domain/reply/applicationa/ReplyServiceTest.java
@@ -4,10 +4,14 @@ import me.golf.blog.domain.board.application.BoardService;
 import me.golf.blog.domain.board.util.GivenBoard;
 import me.golf.blog.domain.member.application.MemberService;
 import me.golf.blog.domain.member.util.GivenMember;
+import me.golf.blog.domain.reply.api.ReplyUpdateRequest;
+import me.golf.blog.domain.reply.domain.persist.Reply;
+import me.golf.blog.domain.reply.domain.persist.ReplyRepository;
 import me.golf.blog.domain.reply.domain.vo.Comment;
 import me.golf.blog.domain.reply.dto.ReplyAllResponse;
 import me.golf.blog.domain.reply.dto.ReplyCreateRequest;
-import org.assertj.core.api.Assertions;
+import me.golf.blog.domain.reply.error.ReplyNotFoundException;
+import me.golf.blog.global.error.exception.ErrorCode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,10 +27,12 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class ReplyServiceTest {
     @Autowired MemberService memberService;
     @Autowired BoardService boardService;
     @Autowired ReplyService replyService;
+    @Autowired ReplyRepository replyRepository;
 
     static Long memberId;
     static Long boardId;
@@ -35,14 +42,14 @@ class ReplyServiceTest {
     void init() {
         memberId = memberService.create(GivenMember.toEntity()).getMemberId();
         boardId = boardService.create(GivenBoard.toEntity(), memberId);
-        replyId = replyService.create(
-                new ReplyCreateRequest(Comment.from("안녕하세요, 테스트입니다. 반가워요")), boardId, memberId);
+        replyId = replyService.create(new ReplyCreateRequest(Comment.from("안녕하세요, 테스트입니다. 반가워요")), boardId, memberId);
     }
 
     @Test
     @DisplayName("정상적으로 댓글 전체를 조회한다.")
     void findAllTest() {
         // given
+
         Pageable pageable = PageRequest.of(0, 10);
 
         for (int i = 0; i < 10; i++) {
@@ -55,5 +62,32 @@ class ReplyServiceTest {
 
         // then
         assertThat(replies.size()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("댓글을 수정할 수 있다.")
+    void updateTest() {
+        // given
+        ReplyUpdateRequest request = new ReplyUpdateRequest(Comment.from("안녕하세요, 테스트입니다. 반가워요 수정본"));
+
+        // when
+        replyService.update(request.getComment(), replyId);
+        Reply reply = replyRepository.findById(replyId)
+                .orElseThrow(() -> new ReplyNotFoundException(ErrorCode.REPLY_NOT_FOUND));
+
+        // then
+        assertThat(reply.getComment().comment()).isEqualTo("안녕하세요, 테스트입니다. 반가워요 수정본");
+        assertThat(reply.getComment().comment()).isNotEqualTo("안녕하세요, 테스트입니다. 반가워요");
+    }
+
+    @Test
+    @DisplayName("댓글을 삭제할 수 있다.")
+    void deleteTest() {
+        // when
+        replyService.deleteById(replyId);
+
+        // then
+        assertThrows(ReplyNotFoundException.class, () -> replyRepository.findById(replyId).orElseThrow(
+                        () -> new ReplyNotFoundException(ErrorCode.REPLY_NOT_FOUND)));
     }
 }
