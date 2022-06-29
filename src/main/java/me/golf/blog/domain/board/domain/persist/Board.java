1d80f4d207e7be546983ec5f9dcb1118c4f9ad37
Developer_GOLF <ilgolc@naver.com>
2022-06-30 00:06:44 +0900

    "게시판 도메인 삭제 전략 변경 is_deleted = true 로 논리적 삭제"

--- src/main/java/me/golf/blog/domain/board/domain/persist/Board.java
+++ src/main/java/me/golf/blog/domain/board/domain/persist/Board.java
@@ -2,27 +2,26 @@ package me.golf.blog.domain.board.domain.persist;
 
 import lombok.*;
 import me.golf.blog.domain.board.domain.vo.BoardImage;
+import me.golf.blog.domain.board.domain.vo.BoardStatus;
 import me.golf.blog.domain.board.domain.vo.Content;
 import me.golf.blog.domain.board.domain.vo.Title;
 import me.golf.blog.domain.boardCount.domain.persist.BoardCount;
 import me.golf.blog.domain.member.domain.persist.Member;
-import me.golf.blog.domain.reply.domain.persist.Reply;
 import me.golf.blog.global.common.BaseEntity;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 @Builder
+@Where(clause = "is_deleted = false")
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "Board", indexes = {
         @Index(name = "index_title", columnList = "title")
 })
 public class Board extends BaseEntity {
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_id", nullable = false)
     private Long id;
@@ -40,9 +39,17 @@ public class Board extends BaseEntity {
     @Embedded
     private BoardImage boardImage;
 
+    @Enumerated(value = EnumType.STRING)
+    @Builder.Default
+    private BoardStatus status = BoardStatus.SAVE;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Builder.Default
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
 
     public Board addMember(final Member member) {
         this.member = member;
@@ -56,5 +63,10 @@ public class Board extends BaseEntity {
 
     public void addBoardCount(final BoardCount boardCount) {
         this.boardCount = boardCount;
+    }
+
+    public void delete() {
+        this.isDeleted = true;
+        recordDeleteTime();
     }
 }
