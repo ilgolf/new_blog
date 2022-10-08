8ed3ac1b0063c62b5b049a53571308876153aca9
Developer.GOLF <ilgolc@naver.com>
2022-10-08 20:17:27 +0900

    리펙토링 : Embedded-Redis 수정

--- src/test/java/me/golf/blog/global/config/TestRedisConfig.java
+++ src/test/java/me/golf/blog/global/config/TestRedisConfig.java
@@ -20,7 +20,10 @@ public class TestRedisConfig {
 
     public TestRedisConfig(@Value("${spring.redis.port}") int redisPort) throws IOException {
         port = redisPort;
-        redisServer = new RedisServer(isRedisRunning()? findAvailablePort() : this.port);
+        redisServer = RedisServer.builder()
+                .port(isRedisRunning() ? findAvailablePort() : this.port)
+                .setting("maxmemory 128M")
+                .build();
         redisServer.start();
     }
 
