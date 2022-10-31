d5c9439a98e2cbb5b42197e2b406f1480f0ac0c3
Devloper.GOLF <ilgolc@naver.com>
2022-10-31 21:10:01 +0900

    hotfix : Security cors 추가

--- src/main/java/me/golf/blog/global/config/SecurityConfig.java
+++ src/main/java/me/golf/blog/global/config/SecurityConfig.java
@@ -43,6 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .cors()
+                .and()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler)
