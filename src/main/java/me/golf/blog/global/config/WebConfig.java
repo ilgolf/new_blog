cdfe010526922d2b62d80cfbb44812fd603d9d9f
Devloper.GOLF <ilgolc@naver.com>
2022-10-31 20:48:33 +0900

    hotfix : header cors issue 문제 해결 추가

--- src/main/java/me/golf/blog/global/config/WebConfig.java
+++ src/main/java/me/golf/blog/global/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
                 .maxAge(3000);
     }
 }
